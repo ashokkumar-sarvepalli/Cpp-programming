42738ebceb3872888f3bdb4b2c904dc0503b237a
Ashokkumar Sarvepalli <ashokkumar.sarvepalli@gmail.com>
2019-01-23 09:40:57 -0500

    Create output.cpp

---
+++ programs file handling by text/output.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+//#include<conio.h>
+#include<fstream>
+#include<string.h>
+#include<ctype.h>
+using namespace std;
+int main()
+{
+	int count=0;
+	char a[80];
+	fstream x("OUT.txt",ios::out);
+	
+	x<<"Ashok"<<endl;
+	x<<"Nishanth"<<endl;
+	x<<"Nisha"<<endl;
+	
+	
+	//getch();
+	return 0;
+	
+	
+	
+}
